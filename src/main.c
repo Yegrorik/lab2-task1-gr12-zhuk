73b5a418f23e7db27e092a444fd84e91e2be1674
Yegrorik <zukegor59@gmail.com>
2026-02-23 11:50:22 +0000

    Добавил ввод еще одной даты

--- src/main.c
+++ src/main.c
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  if (argc != 4) {
-    printf("Использование: %s д1 м1 г1\n", argv[0]);
-    printf("Пример: %s 1 1 2020\n", argv[0]);
-    return 1;
+  if (argc != 7) {
+      printf("Использование: %s д1 м1 г1 д2 м2 г2\n", argv[0]);
+      printf("Пример: %s 1 1 2020 1 1 2021\n", argv[0]);
+      return 1;
   }
   
   int d1 = atoi(argv[1]);
   int m1 = atoi(argv[2]);
   int y1 = atoi(argv[3]);
+  int d2 = atoi(argv[4]);
+  int m2 = atoi(argv[5]);
+  int y2 = atoi(argv[6]);
 
   printf("Исходные данные:\n");
-  printf("Дата: %02d.%02d.%04d\n", d1, m1, y1);
+  printf("Первая дата: %02d.%02d.%04d\n", d1, m1, y1);
+  printf("Вторая дата: %02d.%02d.%04d\n", d2, m2, y2);
   printf("(отсчет с 1 января 1970 года)\n\n");
 }
