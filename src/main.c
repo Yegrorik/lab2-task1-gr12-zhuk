c4525301232b8620804d488af1be11dd4299fbc1
Yegrorik <zukegor59@gmail.com>
2026-02-23 11:43:38 +0000

    Добавил ввод данных через аргументы командной строки

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
-/* hello.c */
 #include <stdio.h>
+#include <stdlib.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+int main(int argc, char *argv[]) {
+  if (argc != 4) {
+    printf("Использование: %s д1 м1 г1\n", argv[0]);
+    printf("Пример: %s 1 1 2020\n", argv[0]);
+    return 1;
+  }
+  
+  int d1 = atoi(argv[1]);
+  int m1 = atoi(argv[2]);
+  int y1 = atoi(argv[3]);
+
+  printf("Исходные данные:\n");
+  printf("Дата: %02d.%02d.%04d\n", d1, m1, y1);
+  printf("(отсчет с 1 января 1970 года)\n\n");
 }
